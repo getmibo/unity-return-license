daa72f8ff0c2f7d3c8ed0194f78a80ae4afa6a7f
Mathijs Kadijk <mkadijk@gmail.com>
2022-01-14 00:13:47 +0100

    Remove check for serial in environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
-
-if [[ -n "$UNITY_SERIAL" ]]; then
-  #
-  # PROFESSIONAL (SERIAL) LICENSE MODE
-  #
-  # This will return the license that is currently in use.
-  #
-  unity-editor \
-    -logFile /dev/stdout \
-    -quit \
-    -returnlicense
-else
-  echo "No UNITY_SERIAL detected! No license was returned."
-fi
+#
+# PROFESSIONAL (SERIAL) LICENSE MODE
+#
+# This will return the license that is currently in use.
+#
+unity-editor \
+  -logFile /dev/stdout \
+  -quit \
+  -returnlicense
